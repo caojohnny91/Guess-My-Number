471ee3430700f81a1a3be42ebb983234dbf41a9e
caojohnny91 <168755794+caojohnny91@users.noreply.github.com>
2024-08-21 09:12:59 -0500

    testing and playing around with very basic DOM manipulation using querySelector

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-'use strict';
+"use strict";
+
+// select element with the class name of message (if it was an id #message) and then selecting the textContent property by using another .
+console.log(document.querySelector(".message").textContent); // Start guessing...
+
+document.querySelector(".message").textContent = " 🎉 Correct Number!";
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
+
+document.querySelector(".guess").value = 23; // target the value property .value because its an input element
+console.log(document.querySelector(".guess").value);
