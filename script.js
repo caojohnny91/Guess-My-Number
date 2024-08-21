c031bf361b405378156099c8f61ba69118b75b78
caojohnny91 <168755794+caojohnny91@users.noreply.github.com>
2024-08-21 09:41:17 -0500

    add eventListener logic if user clicks check btn for no guess input or if guessed number is outside of 1-20 range

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/*
 // select element with the class name of message (if it was an id #message) and then selecting the textContent property by using another .
 console.log(document.querySelector(".message").textContent); // Start guessing...
 
@@ -10,3 +10,20 @@ document.querySelector(".score").textContent = 10;
 
 document.querySelector(".guess").value = 23; // target the value property .value because its an input element
 console.log(document.querySelector(".guess").value);
+*/
+
+// Implement event handler to check btn
+// select check class element, then add the addEventListener method (argument 1 (name of event to listen for): "click", argument 2 (reaction when event happens): function value)
+document.querySelector(".check").addEventListener("click", function () {
+  //   console.log(document.querySelector(".guess").value); // log for testing
+
+  // save value to a variable
+  const guess = Number(document.querySelector(".guess").value); // convert string input to number for later comparisons
+  console.log(guess, typeof guess);
+
+  // implement game logic for no guess or if guess is outside of range
+  if (!guess || guess < 1 || guess > 20) {
+    document.querySelector(".message").textContent =
+      "🛑 Please guess a number between 1 and 20...";
+  }
+});
